ddea5621828345f1e2d47ccad5bbcded64368a72
andzekausk <s23kausandz@venta.lv>
2024-04-30 17:23:00 +0300

    "b. Input all the necessary information in the text fields, buttons, etc, where it is not stated otherwise." done

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,17 @@
+import { HomePage } from "../pageObjects/HomePage";
+
+describe('Demoqa scenario', () => {
+  it('Demoqa scenario ', () => {
+    // a. Open https://demoqa.com/automation-practice-form
+    HomePage.visit();
+    // b. Input all the necessary information in the text fields, buttons, etc, where it is not
+    // stated otherwise.
+    HomePage.firstNameField.type("Andžejs");
+    HomePage.lastNameField.type("Kauškals");
+    HomePage.userEmailField.type("s23kausandz@venta.lv");
+    HomePage.maleRadio.click({force: true});
+    HomePage.userNumberField.type("21234567");
+    HomePage.currentAddressField.type("Grove street");     
+    // c. Set the - Date of Birth - with Calendar widget to - 28th of February, 1930.
+  })
+})
---
+++ cypress/pageObjects/HomePage.js
@@ -0,0 +1,25 @@
+import { BasePage } from "./BasePage";
+export class HomePage extends BasePage{
+    static get url(){
+        return "";
+    }
+    static get firstNameField(){
+        return cy.get('input#firstName');
+    }
+    static get lastNameField(){
+        return cy.get('input#lastName');
+    }
+    static get userEmailField(){
+        return cy.get('input#userEmail');
+    }
+    static get maleRadio(){
+        return cy.get('input#gender-radio-1');
+    }
+    static get userNumberField(){
+        return cy.get('input#userNumber');
+    }
+    static get currentAddressField(){
+        return cy.get('textarea#currentAddress');
+    }
+  }
+  
